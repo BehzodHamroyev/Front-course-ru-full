ef98b26fa7f0962e66e477d61eb64b3112e9b775
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-20 09:49:21 +0500

    add github action

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -22,7 +22,7 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
-      __PROJECT__: JSON.stringify("storybook"),
+      __PROJECT__: JSON.stringify(project),
     }),
   ];
 

23d0127c5d09732ac10c59fea0a8aa2d861eeb8d
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-20 11:33:08 +0500

    build

--- src/features/AuthByUsername/model/slice/LoginSlice.ts
+++ src/features/AuthByUsername/model/slice/LoginSlice.ts
@@ -36,6 +36,5 @@ export const loginSlice = createSlice({
   }
 });
 
-// Action creators are generated for each case reducer function
 export const { actions: loginActions } = loginSlice;
 export const { reducer: loginReducer } = loginSlice;

9dd12c09125c747db44f834304a43eba645320b7
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-20 10:46:16 +0500

    remove node_modules

--- node_modules/babel-plugin-i18next-extract/constants.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const PLUGIN_NAME = "babel-plugin-i18next-extract";
--- node_modules/eslint-plugin-i18next/commitlint.config.js
+++
@@ -1,8 +0,0 @@
-const conventional = require('@commitlint/config-conventional');
-
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', [...conventional.rules['type-enum'][2], 'dev']]
-  }
-};

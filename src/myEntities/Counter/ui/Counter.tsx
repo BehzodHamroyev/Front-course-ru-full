49a33edbbf02888f8cfe784e0300453197d4527e
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-24 10:51:28 +0500

    eddit tsconfig

--- src/myEntities/Counter/ui/Counter.tsx
+++ src/myEntities/Counter/ui/Counter.tsx
@@ -1,9 +1,9 @@
-import { Button } from 'shared/ui/Button/Button';
-import { useDispatch, useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import { counterActions } from '../model/slice/counterSlice';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import React from 'react';
+import { Button } from "shared/ui/Button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { counterActions } from "../model/slice/counterSlice";
+import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
+import React from "react";
 export const Counter = () => {
   const dispatch = useDispatch();
   const counterValue = useSelector(getCounterValue);
@@ -18,20 +18,14 @@ export const Counter = () => {
   };
 
   return (
-        <div>
-            <h1 data-testid="value-title">{counterValue}</h1>
-            <Button
-                onClick={increment}
-                data-testid="increment-btn"
-            >
-                {t('increment')}
-            </Button>
-            <Button
-                data-testid="decrement-btn"
-                onClick={decrement}
-            >
-                {t('decrement')}
-            </Button>
-        </div>
+    <div>
+      <h1 data-testid="value-title">{counterValue}</h1>
+      <Button onClick={increment} data-testid="increment-btn">
+        {t("increment")}
+      </Button>
+      <Button data-testid="decrement-btn" onClick={decrement}>
+        {t("decrement")}
+      </Button>
+    </div>
   );
 };

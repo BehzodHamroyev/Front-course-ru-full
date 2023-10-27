819fa2058e05e89dfec3ac7cb7c4d2aedef835df
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-27 18:10:13 +0500

    change stateScheme

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -40,7 +40,6 @@ export interface ReducerManager {
     reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
     add: (key: StateSchemaKey, reducer: Reducer) => void;
     remove: (key: StateSchemaKey) => void;
-    // true - вмонтирован, false - демонтирован
     getMountedReducers: () => MountedReducers;
 }
 

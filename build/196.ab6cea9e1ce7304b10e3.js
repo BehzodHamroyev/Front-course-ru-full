cd046d6c024822c900b921907d9c6cdc2bd1fb79
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-28 00:05:27 +0500

    remove build

--- build/196.ab6cea9e1ce7304b10e3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[196],{4196:(t,e,r)=>{r.r(e),r.d(e,{default:()=>o});var c=r(5893),u=r(7294),n=r(5443);const o=function(){var t=(0,n.$)().t,e=(0,u.useState)("");return e[0],e[1],(0,c.jsx)("div",{children:t("Главная страница")})}}}]);

f1a86508f66c86e461cd99826ab425515194d850
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-11-03 16:57:58 +0500

    add new testing for router and fast work husky

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,10 +1,8 @@
 import { useParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Page } from '@/widgets/Page';
-import { Text } from '@/shared/ui/Text/Text';
-import { EditableProfileCard } from '@/features/editableProfileCard';
 import { VStack } from '@/shared/ui/Stack';
+import { EditableProfileCard } from '@/features/editableProfileCard';
 
 interface ProfilePageProps {
     className?: string;
@@ -14,7 +12,10 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
   const { id } = useParams<{ id: string }>();
 
   return (
-      <Page className={classNames('', {}, [className])}>
+      <Page
+            data-testid="ProfilePage"
+            className={classNames('', {}, [className])}
+        >
           <VStack gap="16" max>
               <EditableProfileCard id={id} />
           </VStack>
--- src/shared/ui/AppImage/AppImage.stories.tsx
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Page } from './Page';
+import { AppImage } from './AppImage';
 
 export default {
-  title: 'shared/Page',
-  component: Page,
+  title: 'shared/AppImage',
+  component: AppImage,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof Page>;
+} as ComponentMeta<typeof AppImage>;
 
-const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+const Template: ComponentStory<typeof AppImage> = (args) => <AppImage {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {};

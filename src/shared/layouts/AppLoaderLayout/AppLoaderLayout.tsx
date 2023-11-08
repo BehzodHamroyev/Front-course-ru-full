3b06a8ec1145c6062907d8128abc2e807bec7c94
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-11-08 16:53:24 +0500

    last commit

--- src/shared/layouts/AppLoaderLayout/AppLoaderLayout.tsx
+++ src/shared/layouts/AppLoaderLayout/AppLoaderLayout.tsx
@@ -5,14 +5,14 @@ import { MainLayout } from '../MainLayout';
 import cls from './AppLoaderLayout.module.scss';
 
 export const AppLoaderLayout = memo(() => {
-    return (
-        <MainLayout
-            header={
+  return (
+      <MainLayout
+            header={(
                 <HStack className={cls.header}>
                     <Skeleton width={40} height={40} border="50%" />
                 </HStack>
-            }
-            content={
+              )}
+            content={(
                 <VStack gap="16" style={{ height: '100%' }}>
                     <Skeleton width="70%" height={32} border="16px" />
                     <Skeleton width="40%" height={20} border="16px" />
@@ -21,8 +21,8 @@ export const AppLoaderLayout = memo(() => {
                     <Skeleton width="80%" height="40%" border="16px" />
                     <Skeleton width="80%" height="40%" border="16px" />
                 </VStack>
-            }
+              )}
             sidebar={<Skeleton border="32px" width={220} height="100%" />}
         />
-    );
+  );
 });
